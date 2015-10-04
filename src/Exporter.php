b7985117208b0ad3bac0ca622224afdc498e6454
mattcannon <matt@mattcannon.uk>
2015-10-04 16:35:41 +0100

    update to add cvs writing to csv

--- src/Exporter.php
+++ src/Exporter.php
@@ -2,9 +2,13 @@
 
 namespace MattCannon\Lang2Csv;
 
+use League\Csv\Writer;
+use SplFileObject;
+
 class Exporter
 {
     private $baseDirectory = './';
+
     private function __construct()
     {
     }
@@ -18,6 +22,36 @@ class Exporter
 
     public function exportLanguageTo($language, $destination)
     {
-        // TODO: write logic here
+        $translations = $this->getValuesFromFiles($language);
+        $translations = array_dot($translations);
+        $rows = [];
+        foreach($translations as $key => $value){
+            $rows[] = [$key,$value];
+        }
+        $destinationPath = $this->getDestinationPathForCsv($destination, $language);
+//        $this->writeToCsv($translations, $destinationPath);
+        
+    }
+
+    public function getValuesFromFiles($languageCode)
+    {
+        $basePath = $this->baseDirectory.$languageCode.'/';
+        $files = glob($basePath.'*.php');
+        $values = [];
+        foreach($files as $file){
+            $values[substr($file,strlen($basePath),-4)] = include($file);
+        }
+        return $values;
+    }
+
+    public function getDestinationPathForCsv($destination, $language)
+    {
+        return $destination.'/'.$language.'_export.csv';
+    }
+
+    public function writeToCsv($rows, $destinationPath)
+    {
+        $csv = Writer::createFromPath($destinationPath,'w+');
+        $csv->insertAll($rows);  
     }
 }
